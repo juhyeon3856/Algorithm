252166a21f6d656c7a03d5036edde47bdc23eef3
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-01-24 19:19:24 +0900

    [Silver III] Title: N과 M (5), Time: 364 ms, Memory: 42664 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N, R;
+	public static int[] nums;
+	public static int[] p;
+	public static boolean[] isVisited;
+	
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		p = new int[N];
+		isVisited = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(p);
+		nums = new int[R];
+		perm(0, 0);
+		System.out.println(sb);
+	}
+
+	private static void perm(int depth, int start) throws Exception  {
+		if(R==depth) {
+			for (int i = 0; i < R; i++) {
+				sb.append(nums[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if(isVisited[i]) continue;
+			isVisited[i] = true;
+			nums[depth] = p[i];
+			perm(depth+1, i);
+			isVisited[i] = false;
+			nums[depth] = 0;
+		}
+	}
+}
