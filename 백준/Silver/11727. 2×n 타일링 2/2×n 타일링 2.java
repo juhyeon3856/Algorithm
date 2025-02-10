512b98727f72e9b29822b66bf7f20fc62fa0d742
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-02-10 15:20:51 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 172 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static int[] l;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		l = new int[N + 2];
+		l[1] = 1;
+		l[2] = 3;
+		System.out.println(f(N));
+
+	}
+
+	public static int f(int n) {
+		if (l[n] == 0) {
+			l[n] = (f(n - 1) + 2*f(n - 2))%10007;
+			return l[n];
+		} else {
+			return l[n];
+		}
+	}
+
+}
