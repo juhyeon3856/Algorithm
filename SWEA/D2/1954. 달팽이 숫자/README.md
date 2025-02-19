862a846f965e6e888e6d07d4da765ed85640cd05
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-02-19 12:51:17 +0900

    [D2] Title: 달팽이 숫자, Time: 121 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/README.md
+++ SWEA/D2/1954. 달팽이 숫자/README.md
@@ -1,14 +1,14 @@
-# [D2] 달팽이 숫자 - 1954  
+# [D2] 달팽이 숫자 - 1954 
 
 [문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PobmqAPoDFAUq) 
 
 ### 성능 요약
 
-메모리: 20,968 KB, 시간: 144 ms, 코드길이: 1,230 Bytes
+메모리: 26,880 KB, 시간: 121 ms, 코드길이: 1,019 Bytes
 
 ### 제출 일자
 
-2025-01-07 21:11
+2025-02-19 12:51
 
 
 
--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,45 +1,51 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	static int N;
+	static int[][] map;
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		int T = sc.nextInt();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            int N = sc.nextInt();
-            int[][] moves = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-            int[][] board = new int[N][N];
-            int num = 1;
-            int moveIdx = 0;
-            int[] idx = {0, -1};
-            
-            while(num <= N*N){
-                int x = idx[0] + moves[moveIdx][0];
-                int y = idx[1] + moves[moveIdx][1];
-                if(x>=0 && x<N && y>=0 && y<N && board[x][y] == 0){
-                    board[x][y] = num;
-                    num++;
-                    idx[0] = x;
-                    idx[1] = y;
-                } else{
-                    moveIdx = (moveIdx+1)%4;
-                }
-            }
-            
-            System.out.println("#"+test_case);
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    System.out.print(board[i][j] + " ");
-            	}
-                System.out.println();
-            }
-            
-            
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			int pow = N * N;
+			int[] idx = { 0, -1 };
+			int num = 1;
+			int d = 0;
+			map = new int[N][N];
+
+			int[] dr = { 0, 1, 0, -1 };
+			int[] dc = { 1, 0, -1, 0 };
+
+			while (num <= pow) {
+				int nr = idx[0] + dr[d];
+				int nc = idx[1] + dc[d];
+				if (check(nr, nc) && map[nr][nc] == 0) {
+					map[nr][nc] = num;
+					num++;
+					idx[0] += dr[d];
+					idx[1] += dc[d];
+				} else {
+					d = (d + 1) % 4;
+				}
+			}
+
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(t).append("\n");
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					sb.append(map[i][j]).append(" ");
+				}
+				sb.append("\n");
+			}
+			System.out.print(sb);
 		}
 	}
+
+	private static boolean check(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
+
 }
