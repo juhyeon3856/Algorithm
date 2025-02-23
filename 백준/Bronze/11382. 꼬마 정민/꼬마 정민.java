d33cf5c25e6108a85812f3683d9ec12132d35ede
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-02-23 16:30:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 96 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    static public void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A+B+C);
+    }
+}
