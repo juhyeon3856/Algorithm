2fab77d620b93ecb67a05ceca0121639c72bdf03
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-03 10:15:32 +0900

    [Gold V] Title: 토마토, Time: 492 ms, Memory: 127980 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,19 +1,24 @@
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int M = sc.nextInt(), N = sc.nextInt();
+	public static void main(String[] args) throws Exception{
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 		int[][] box = new int[N][M];
 		int count = 0;
 		Queue<int[]> queue = new LinkedList<>();
 
 		for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				box[i][j] = sc.nextInt();
+				box[i][j] = Integer.parseInt(st.nextToken());
 				if (box[i][j] == 0)
 					count++;
 				if (box[i][j] == 1)
@@ -27,8 +32,8 @@ public class Main {
 
 		while (!queue.isEmpty()) {
 			int[] index = queue.poll();
-			for (int[] _index : new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } }) {
-				int r = index[0] + _index[0], c = index[1] + _index[1];
+            for(int d = 0; d < 4 ; d++){
+				int r = index[0] + dr[d], c = index[1] + dc[d];
 				if (r >= 0 && r < N && c >= 0 && c < M && box[r][c] == 0) {
 					box[r][c] = 1;
 					count--;
