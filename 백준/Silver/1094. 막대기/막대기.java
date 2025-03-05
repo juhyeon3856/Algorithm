3a704a839e01f62b8d949eb943be8f728e2bca72
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-06 08:55:37 +0900

    [Silver V] Title: 막대기, Time: 92 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = Integer.toBinaryString(sc.nextInt());
-		int answer = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if(str.charAt(i)=='1') 
-				answer++;
-		}
-		System.out.println(answer);
+		System.out.println(Integer.bitCount(sc.nextInt()));
 	}
 }
