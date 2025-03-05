866ea7f942c077b9e4cdc3a51e939cdb1c7ef75a
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-05 17:13:59 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Main  {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		List<Character> stack = new LinkedList<>();
 		int N = str.length();
+		char[] stack = new char[N];
+		int top = 0;
 		char[] strList = new char[N];
 		String answer = "";
 
@@ -22,22 +20,22 @@ public class Main  {
 
 		for (int i = 0; i < N; i++) {
 			char c = strList[i];
-			if (stack.isEmpty()) {
-				stack.add(0, c);
-			} else if (c == stack.get(0)) {
-				stack.remove(0);
+			if (top == 0) {
+				stack[top++] = c;
+			} else if (c == stack[top-1]) {
+				top--;
 				answer += c;
 			} else {
-				stack.add(0, c);
+				stack[top++] = c;
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(answer).reverse();
-		if (stack.size() == 0) {
+		if (top == 0) {
 			System.out.println(answer + sb);
-		} else if (stack.size() == 1) {
-			System.out.println(answer + stack.get(0) + sb);
+		} else if (top == 1) {
+			System.out.println(answer + stack[0] + sb);
 		} else {
 			System.out.println("I'm Sorry Hansoo");
 		}
