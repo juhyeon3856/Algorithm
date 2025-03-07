dc24e42a63b9da6306daff23ccd50eea85de72f5
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-07 09:02:00 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 68 ms, Memory: 12076 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,5 +1,8 @@
 import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
 
@@ -11,13 +14,15 @@ public class Main {
 	static boolean[][][] visited;
 	static ArrayDeque<int[]> queue;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
 		p = new int[N];
 
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			p[i] = sc.nextInt();
+			p[i] = Integer.parseInt(st.nextToken());
 		}
 		queue = new ArrayDeque<>();
 		queue.offer(p);
@@ -34,7 +39,6 @@ public class Main {
 		// 입력 완료 로직 시작
 		bfs();
 		System.out.println(answer);
-		sc.close();
 	}
 
 	private static void bfs() {
