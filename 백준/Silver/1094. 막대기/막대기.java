1c4c940481c84f711483a00024069a59bc883f57
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-10 10:57:52 +0900

    [Silver V] Title: 막대기, Time: 64 ms, Memory: 11420 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int i = 0, c;
 		while ((c = System.in.read()) >= '0' && c <= '9') {
-			i = i * 10 + (c - '0'); // 입력된 숫자를 변환
+			i = i * 10 + (c &15); // 입력된 숫자를 변환
 		}
 		i = i - ((i >> 1) & 0x55555555);
 		i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
