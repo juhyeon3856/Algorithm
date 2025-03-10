38a57745a26a8fc56463abe7342dce15b03c176b
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-03-10 10:27:13 +0900

    [Silver V] Title: 막대기, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,7 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println(Integer.bitCount(sc.nextInt()));
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int i = Integer.parseInt(br.readLine());
+		i = i - ((i >>> 1) & 0x55555555);
+		i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+		i = (i + (i >>> 4)) & 0x0f0f0f0f;
+		System.out.println(i & 0x3f);
 	}
 }
