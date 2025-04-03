1b19eb595f0ed81161d156ef27d9f084c04ce7ca
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-04-03 10:37:10 +0900

    [Silver III] Title: 이친수, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,10 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
