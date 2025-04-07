600305100b31842104294dd20ce380adb1ebb379
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-04-07 10:40:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 13196 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, W;
-	static int[][] dp;
+	static int[] dp;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,19 +13,18 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		W = Integer.parseInt(st.nextToken());
 
-		dp = new int[N + 1][W + 1];
+		dp = new int[W + 1];
 
-//		System.out.println();
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int w = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			for (int j = 1; j <= W; j++) {
-				dp[i][j] = j >= w ? Math.max(dp[i - 1][j], v + dp[i - 1][j - w]) : dp[i - 1][j];
+			for (int j = W; j > 0; j--) {
+				dp[j] = j >= w ? Math.max(dp[j], v + dp[j - w]) : dp[j];
 			}
 //			System.out.println(Arrays.toString(dp[i]));
 		}
-		System.out.println(dp[N][W]);
+		System.out.println(dp[W]);
 
 	}
 }
