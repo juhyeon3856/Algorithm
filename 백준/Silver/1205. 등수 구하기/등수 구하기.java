0bda394c33c0aa16e26c72dbc914e966c2646cf8
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-04-28 16:33:05 +0900

    [Silver IV] Title: 등수 구하기, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,6 +21,8 @@ public class Main {
 				answer++;
 			} else if (score == my) {
 				same++;
+			} else {
+				break;
 			}
 		}
 		if (answer + same > P) {
