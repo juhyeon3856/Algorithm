dd63472c2d3430a5f9027c95890977977c1b1189
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-05-23 21:38:59 +0900

    [Silver III] Title: 어린 왕자, Time: 92 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.java
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.java
@@ -18,20 +18,16 @@ public class Main {
 			int y2 = Integer.parseInt(st.nextToken());
 
 			int N = Integer.parseInt(br.readLine());
-			boolean[] contain1 = new boolean[N];
-			boolean[] contain2 = new boolean[N];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				int xo = Integer.parseInt(st.nextToken());
 				int yo = Integer.parseInt(st.nextToken());
 				int r = Integer.parseInt(st.nextToken());
-				contain1[i] = (x1 - xo) * (x1 - xo) + (y1 - yo) * (y1 - yo) < r * r;
-				contain2[i] = (x2 - xo) * (x2 - xo) + (y2 - yo) * (y2 - yo) < r * r;
-			}
-			for (int i = 0; i < N; i++) {
-				if (contain1[i] ^ contain2[i]) {
+				if ((x1 - xo) * (x1 - xo) + (y1 - yo) * (y1 - yo) < r * r
+						^ (x2 - xo) * (x2 - xo) + (y2 - yo) * (y2 - yo) < r * r) {
 					answer++;
 				}
+				;
 			}
 			sb.append(answer).append("\n");
 		}
