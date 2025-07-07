5762c096423c8bcc8681008d93624379c6cd04d5
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-07-08 00:17:59 +0900

    [Bronze I] Title: 단어 공부, Time: 188 ms, Memory: 24344 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] count;
+	static char answer;
+	static int maxCount;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		str = str.toUpperCase();
+
+		count = new int[26];
+
+		for (int i = 0; i < str.length(); i++) {
+			count[str.charAt(i) - 'A']++;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (count[i] > maxCount) {
+				maxCount = count[i];
+				answer = (char) (i + 'A');
+			} else if (count[i] == maxCount) {
+				answer = '?';
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
