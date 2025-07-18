6d2ca023a1227f2612aaac6f6fa72f5d7b4ac0f8
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-07-18 14:58:59 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -4,33 +4,11 @@ input = sys.stdin.readline
 
 N = int(input())
 answer = 0
-data = [float(input()) for _ in range(N)]
+dp = 0
 
-def sol(s, e):
-  n = e-s
-  maxList = [0] * (N+2)
-  minList = [1] * (N+2)
-  multList = [1] * (N+1)
-  result = 0
-  for i in range(s, e):
-    multList[i+1] = multList[i] * data[i]
-
-  for i in range(e, s, -1):
-    maxList[i] = max(multList[i], maxList[i+1])
-
-  for i in range(s+1, e+1):
-    minList[i] = min(multList[i], minList[i-1])
-
-  for i in range(s+1, e+1):
-    result = max(result, maxList[i]/minList[i-1])
-  
-  return round(result, 3)
-
-s = 0
 for i in range(N):
-  if data[i] == 0:
-    if(s < i): answer = max(answer, sol(s, i))
-    s = i+1
-if s < N : answer = max(answer, sol(s,N))
-  
+  num = float(input())
+  dp = max(dp*num, num)
+  answer = max(answer, dp)
+
 print(f"{answer:.3f}")
