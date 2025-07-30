59034d2b4044151cf3d4eede2ffb08cec109c508
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-07-30 12:25:57 +0900

    [Silver II] Title: 촌수계산, Time: 116 ms, Memory: 112308 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,33 @@
+# 아이디어
+# bfs거리 계산
+
+V = int(input())
+S, G = map(int, input().split())
+E = int(input())
+adj = [[] for _ in range(V+1)]
+visited = [0] * (V+1)
+
+for _ in range(E):
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)    # 양방향
+
+ans = -1
+# 입력 완료! 로직 시작!
+from collections import deque
+
+queue = deque([S])
+visited[S] = 1
+
+while queue:
+    q = queue.pop()
+    if q == G:
+        ans = visited[G] - 1
+        break
+    for nxt in adj[q]:
+        if visited[nxt]:
+            continue
+        queue.appendleft(nxt)
+        visited[nxt] = visited[q] + 1
+
+print(ans)
