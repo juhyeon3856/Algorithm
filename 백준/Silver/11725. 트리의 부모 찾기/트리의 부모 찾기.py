0b8edbed172947fc8b4641cf0c1bac63f71eb5a3
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-04 15:38:25 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 276 ms, Memory: 124872 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,5 +1,7 @@
-import sys
-sys.setrecursionlimit(110000)
+# 주석은 dfs, 실행 bfs
+from collections import deque
+# import sys
+# sys.setrecursionlimit(110000)
 
 N = int(input())
 
@@ -7,12 +9,12 @@ adj = [[] for _ in range(N + 1)]
 ans = [0] * (N + 1)  # 각각의 부모 노드 저장
 
 
-def dfs(prev):
-    for nxt in adj[prev]:
-        if ans[nxt]:  # 이미 부모 있음
-            continue  # visited와 같은 역할
-        ans[nxt] = prev
-        dfs(nxt)
+# def dfs(prev):
+#     for nxt in adj[prev]:
+#         if ans[nxt]:  # 이미 부모 있음
+#             continue  # visited와 같은 역할
+#         ans[nxt] = prev
+#         dfs(nxt)
 
 
 for _ in range(1, N):
@@ -21,7 +23,19 @@ for _ in range(1, N):
     adj[e].append(s)
 
 # 입력 완료! 로직 시작
-dfs(1)  # root node = 1
+
+# dfs(1)  # root node = 1
+
+queue = deque([1])
+ans[1] = -1     # 부모 없지만 visited역할 하기 위해서
+
+while queue:
+    prev = queue.popleft()
+    for nxt in adj[prev]:
+        if ans[nxt]:    # 이미 부모 있으면
+            continue    # visited와 같은 역할
+        ans[nxt] = prev
+        queue.append(nxt)
 
 # 정답 출력
 for i in range(2, N + 1):
