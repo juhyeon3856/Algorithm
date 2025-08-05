b82058e0c754993057bd097966856cd39cae9bc1
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-05 16:59:43 +0900

    [Silver I] Title: 그래프 탐색 2, Time: 128 ms, Memory: 116348 KB -BaekjoonHub

--- 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
+++ 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
@@ -1,14 +1,16 @@
 # bfs한번만 돌리고 prev, nxt를 저장해서 연결 될 때 마다 업데이트 하는 방식
 # 시간 복잡도 (N+M+Q)*Q에서 (M+Q)*Q로 줄어들 듯 -> 사실상 (N+Q)*Q일수도?
+# 입출력 수정# 입출력 수정(jos***97님 참고)
 
 from collections import deque
 
-N, M = map(int, input().split())
+inp = open(0)
+N, M = map(int, next(inp).split())
 adj = [[] for _ in range(N + 1)]
 
 # 인접리스트 만들기
 for _ in range(M):
-    a, b = map(int, input().split())
+    a, b = map(int, next(inp).split())
     adj[a].append(b)
     adj[b].append(a)  # 양방향
 
@@ -38,10 +40,11 @@ def update(n):  # p -> n으로 연결
             queue.append(nn)  # 최단거리이므로 루프 가능성 없음
 
 
-Q = int(input())
+Q = int(next(inp))
 cnt = init()  # bfs는 이때 한번만 돌림
+ans = []
 for _ in range(Q):  # 500
-    a, b = map(int, input().split())
+    a, b = map(int, next(inp).split())
     adj[a].append(b)  # a에 b 연결하기 : a -> b
     adj[b].append(a)  # b에 a 연결하기 b -> a
     if cnt[a] == cnt[b] == -1:  # 둘다 -1인 경우 서로 연결
@@ -58,5 +61,7 @@ for _ in range(Q):  # 500
     elif cnt[a] + 1 < cnt[b]:
         cnt[b] = cnt[a] + 1
         update(b)
+    ans.append(cnt[1:])
+    # print(*cnt[1:])
 
-    print(*cnt[1:])
+print('\n'.join((' '.join(map(str, i)) for i in ans)))
