e51a9977bc4188b7de76729ced8437730c7133cc
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-06 15:06:40 +0900

    [Silver III] Title: 근손실, Time: 116 ms, Memory: 111412 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,3 +1,4 @@
+# flag(144ms)와 visited차이 보기
 N, K = map(int, input().split())
 
 arr = list(map(lambda x: int(x) - K, input().split()))
@@ -10,10 +11,13 @@ def perm(depth, tot, flag):
         ans[0] += 1
         return
     for i in range(N):
-        if flag & 1 << i: continue
+        if visited[i]: continue
+        visited[i] = 1
         perm(depth + 1, tot + arr[i], flag | 1 << i)
+        visited[i] = 0
 
 
 ans = [0]
+visited = [0] * N
 perm(0, 0, 0)
 print(ans[0])
