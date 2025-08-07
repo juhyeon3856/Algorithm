4839db3848ad537c6a3e7620fe3bb729555f158e
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-07 16:18:41 +0900

    [Gold IV] Title: N-Queen, Time: 3084 ms, Memory: 123252 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,20 +1,35 @@
 N = int(input())
 
 # 풀이 [1] 룩업테이블
-ver, diagU, diagD = [0] * N, [0] * (N + N - 1), [0] * (N + N - 1)
+# ver, diagU, diagD = [0] * N, [0] * (N + N - 1), [0] * (N + N - 1)
+#
+# def dfs(r):
+#     if r == N:
+#         ans[0] += 1
+#         return
+#     for c in range(N):
+#         if ver[c] or diagU[r+c] or diagD[r-c]:  # 같은 열이나, 대각선에 이미 있으면
+#             continue    # 패스
+#         ver[c] = diagU[r+c] = diagD[r-c] = 1
+#         dfs(r+1)
+#         ver[c] = diagU[r + c] = diagD[r - c] = 0
+#
+#
+# ans = [0]
+# dfs(0)
+# print(ans[0])
 
-def dfs(r):
+
+# 풀이 [2] bitmask
+def dfs(r, ver, diagU, diagD):
     if r == N:
         ans[0] += 1
         return
+    flag = ver | diagU | diagD
     for c in range(N):
-        if ver[c] or diagU[r+c] or diagD[r-c]:  # 같은 열이나, 대각선에 이미 있으면
-            continue    # 패스
-        ver[c] = diagU[r+c] = diagD[r-c] = 1
-        dfs(r+1)
-        ver[c] = diagU[r + c] = diagD[r - c] = 0
-
+        if flag & 1<<c: continue    # 놓을 수 없는 곳이면
+        dfs(r+1, ver | 1<<c, (diagU | 1<<c)<<1, (diagD | 1<<c) >> 1)
 
 ans = [0]
-dfs(0)
+dfs(0, 0, 0, 0)
 print(ans[0])
