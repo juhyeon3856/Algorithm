8673367a4a837d8799a10a0877cabc5a6ad50b8d
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-07 16:11:29 +0900

    [Gold IV] Title: N-Queen, Time: 4844 ms, Memory: 119380 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+# 풀이 [1] 룩업테이블
+ver, diagU, diagD = [0] * N, [0] * (N + N - 1), [0] * (N + N - 1)
+
+def dfs(r):
+    if r == N:
+        ans[0] += 1
+        return
+    for c in range(N):
+        if ver[c] or diagU[r+c] or diagD[r-c]:  # 같은 열이나, 대각선에 이미 있으면
+            continue    # 패스
+        ver[c] = diagU[r+c] = diagD[r-c] = 1
+        dfs(r+1)
+        ver[c] = diagU[r + c] = diagD[r - c] = 0
+
+
+ans = [0]
+dfs(0)
+print(ans[0])
