bab300af334ca22da82651c79ec777e58bd4f37b
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-08-12 15:07:20 +0900

    [Silver I] Title: 회전초밥, Time: 348 ms, Memory: 159520 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.py
+++ 백준/Silver/28107. 회전초밥/회전초밥.py
@@ -1,4 +1,5 @@
-from collections import deque
+# from collections import deque
+from heapq import heappop, heappush, heapify
 
 # 입력
 N, M = map(int, input().split())
@@ -6,11 +7,12 @@ Alst = [list(map(int, input().split()))[1:] for _ in range(N)]
 B = list(map(int, input().split()))  # 초밥 종료
 
 # 전처리
-arr = [deque([]) for _ in range(200_001)]  # 조밥 종류 별 대기자 명단 -> 작은 순
+arr = [[] for _ in range(200_001)]  # 조밥 종류 별 대기자 명단 -> 작은 순
 
 for i in range(N):
     for n in Alst[i]:
-        arr[n].append(i)  # n번 초밥 대기줄에 i추가
+        # arr[n].append(i)  # n번 초밥 대기줄에 i추가
+        heappush(arr[n], i)
 
 ans = [0] * N  # i번째 손님이 먹은 초밥 수
 
@@ -18,7 +20,8 @@ ans = [0] * N  # i번째 손님이 먹은 초밥 수
 # 아이디어 : bi번째 초밥이 나오면 arr[bi]의 첫 대기자에게 초밥 주기
 for b in B:
     if arr[b]:  # 대기자가 있으면
-        ans[arr[b].popleft()] += 1
+        # ans[arr[b].popleft()] += 1
+        ans[heappop(arr[b])] += 1
 
 # 정답 출력
 print(*ans)
