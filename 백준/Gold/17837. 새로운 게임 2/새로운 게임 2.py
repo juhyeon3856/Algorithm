c8d2acad127158b98413f164688d0a7937cd3f4a
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-09-25 20:03:20 +0900

    [Gold II] Title: 새로운 게임 2, Time: 128 ms, Memory: 111928 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
@@ -1,119 +1,62 @@
-# 입력
+
+
+# 7시 12분 시작
+# 17분 코드 작성 시작
+# 31분 디버깅 시작
+# 56분 제출
+
+
 N, K = map(int, input().split())
-# 체스판을 벗어나는 경우에는 파란색과 같은 경우이다. -> 패딩
-board_arr = [[2] * (N + 2)] + [[2] + list(map(int, input().split())) + [2] for _ in range(N)] + [[2] * (N + 2)]
+board = [[2] * (N + 2)] + [[2] + list(map(int, input().split())) + [2] for _ in range(N)] + [[2] * (N + 2)]
+stone_lst = [0] + [list(map(int, input().split())) for _ in range(K)]
 
-# 우 좌 상 하
 dr = [100, 0, 0, -1, 1]
-dc = [100, 1, -1, 0, 0]
-dt = [100, 1, -1, 1, -1]  # 방향 반대로 가중치(d에 더함)
-
-
-class Node:
-    def __init__(self, id, r, c, d):
-        self.id = id
-        self.r = r
-        self.c = c
-        self.d = d
-
-    def __repr__(self):
-        return f"{self.id}{dir_dict[self.d]}"
-
-    def move(self, dir):
-        self.r, self.c = self.r + dr[dir], self.c + dc[dir]
-
-    def turn(self):
-        self.d = self.d + dt[self.d]
-
-
-node_arr = [[[] for _ in range(N + 2)] for _ in range(N + 2)]
-node_lst = []
-
+dc = [100, 1, -1, 0, 0]  # 우좌상하
+dt = [100, 2, 1, 4, 3]
+stone_arr = [[[] for _ in range(N + 2)] for _ in range(N + 2)]
 for i in range(1, K + 1):
-    inp_r, inp_c, inp_d = map(int, input().split())
-    new_node = Node(i, inp_r, inp_c, inp_d)
-    node_lst.append(new_node)
-    node_arr[inp_r][inp_c].append(new_node)
+    gr, gc, gd = stone_lst[i]
+    stone_arr[gr][gc].append([i, gd])
+
+    # 로직 시작
 
 
-def find_height(node):
-    # 몇층인지 찾기
-    result = 0
-    for arr_node in node_arr[node.r][node.c]:
-        if node == arr_node:
-            break
-        result += 1
-    else:
-        print("NOOOOOOOOOOOOOOO")
-    return result
+def find_num(num):
+    for r in range(N + 2):
+        for c in range(N + 2):
+            for vi in range(len(stone_arr[r][c])):
+                if stone_arr[r][c][vi][0] == num:
+                    return r, c, vi, stone_arr[r][c][vi][1]
 
 
-def do_move(flst, fnr, fnc, fcr, fcc, fdir):
-    for move_node in flst:  # node이동시키기
-        move_node.move(fdir)
+def sol():
+    for time in range(1, 1001):
+        for i in range(1, K + 1):
+            gr, gc, hi, gd = find_num(i)
+            nr, nc = gr + dr[gd], gc + dc[gd]
+            if board[nr][nc] == 2:
+                stone_arr[gr][gc][hi][1] = dt[gd]  # 방향 전환
+                gd = stone_arr[gr][gc][hi][1]
+                nr, nc = gr + dr[gd], gc + dc[gd]
+            # 이제 진짜 이동
+            # for vi in range(len(stone_arr[gr][gc])):
+            #     if stone_arr[gr][gc][vi] == i:
+            #         hi = vi
+            #         break
+            if board[nr][nc] == 0:
+                move_lst = stone_arr[gr][gc][hi:]
+                stone_arr[gr][gc] = stone_arr[gr][gc][:hi]
+                stone_arr[nr][nc].extend(move_lst)
+                if len(stone_arr[nr][nc]) >= 4:
+                    return time
 
-    node_arr[fnr][fnc].extend(flst)  # 다음 맵에 붙이기
-    node_arr[fcr][fcc] = node_arr[fcr][fcc][:height]  # 기존 맵에서 제거
-    if len(node_arr[nr][nc]) >= 4:
-        return True
-    return False
+            elif board[nr][nc] == 1:
+                move_lst = stone_arr[gr][gc][hi:]
+                stone_arr[gr][gc] = stone_arr[gr][gc][:hi]
+                stone_arr[nr][nc].extend(move_lst[::-1])
+                if len(stone_arr[nr][nc]) >= 4:
+                    return time
+    return -1
 
 
-dir_dict = {1: '→', 2: '←', 3: '↑', 4: '↓'}
-
-
-def print_arr():
-    print("======================================================================")
-    for llst in node_arr:
-        for lllst in llst:
-            end_str = '\t' * (4 - len(lllst))
-            print(lllst, end=end_str)
-        print()
-        # print(*llst, sep='\t')
-
-
-debug = 0
-
-ans = 0
-is_end = False  # 4개 이상이면 True로 바꾸기
-# 로직
-while not is_end:  # 턴이 진행되던 중에 말이 4개 이상 쌓이는 순간 게임이 종료
-    ans += 1
-    if ans > 1000:
-        ans = -1
-        break
-
-    # 말 이동
-    for cur_node in node_lst:
-        # print_arr()
-        cr, cc, cd = cur_node.r, cur_node.c, cur_node.d
-        nr, nc = cr + dr[cd], cc + dc[cd]
-
-        if board_arr[nr][nc] == 0:
-            height = find_height(cur_node)
-            move_lst = node_arr[cr][cc][height:]
-            is_end = do_move(move_lst, nr, nc, cr, cc, cd)
-
-        elif board_arr[nr][nc] == 1:
-            height = find_height(cur_node)
-            move_lst = node_arr[cr][cc][height:]
-            is_end = do_move(move_lst[::-1], nr, nc, cr, cc, cd)
-
-        elif board_arr[nr][nc] == 2:
-            cur_node.turn()
-            cr, cc, cd = cur_node.r, cur_node.c, cur_node.d
-            nr, nc = cr + dr[cd], cc + dc[cd]
-            if board_arr[nr][nc] == 0:
-                height = find_height(cur_node)
-                move_lst = node_arr[cr][cc][height:]
-                is_end = do_move(move_lst, nr, nc, cr, cc, cd)
-
-            elif board_arr[nr][nc] == 1:
-                height = find_height(cur_node)
-                move_lst = node_arr[cr][cc][height:]
-                is_end = do_move(move_lst[::-1], nr, nc, cr, cc, cd)
-
-        if is_end:
-            break
-
-print(ans)
+print(sol())
